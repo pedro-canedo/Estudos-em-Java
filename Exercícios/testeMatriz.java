522c761ff7c8bc1b43229ca87b270056a2343347
Pedro Augusto Canedo Araujo Obalhe <82132100+pedrim775@users.noreply.github.com>
2021-08-21 10:48:59 -0300

    Code-fail

---
+++ Exercícios/testeMatriz.java
@@ -0,0 +1,25 @@
+import java.util.scanner;
+
+class testeMatriz {
+    public static void main(String[] args) ｛
+
+        int i, j;
+
+        int m[][] = new int[3][3];
+
+                
+
+        for (i = 0; i < 3; i++)
+
+            for (j = 0; j < 3; j++)
+
+                m[i][j] = i + j;
+
+        System.out.println();
+
+        System.out.println("Diagonal principal");
+
+    ｝            
+
+}
+//calculo de matriz//
