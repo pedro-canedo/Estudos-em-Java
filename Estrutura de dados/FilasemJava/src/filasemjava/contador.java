07289069a2d3a8c8c32ab4d3bb4858aa4f3c9790
Pedro Augusto Canedo Araujo Obalhe <82132100+pedro-canedo@users.noreply.github.com>
2021-08-25 13:26:26 -0300

    Estrutura de Dados

---
+++ Estrutura de dados/FilasemJava/src/filasemjava/contador.java
@@ -0,0 +1,28 @@
+package filasemjava;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author pedro
+ */
+public class contador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Queue<Integer>fila = new LinkedList<>();
+        for(int i= 0; i < 20; i+=2)
+            fila.add(i);
+        for(int i = 0;i < 10; i++)
+            fila.remove();
+        if(fila.isEmpty())
+            System.out.println("A fila está vazia! ");
+        else
+            System.out.println("A fila não está vaizia! ");
+        
+    }
+    
+}
