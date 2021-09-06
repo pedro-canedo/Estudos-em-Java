bbf8ec7db5a522e9b172c5c3ba73646431006cf3
Pedro Augusto Canedo Araujo Obalhe <82132100+pedrim775@users.noreply.github.com>
2021-09-05 23:00:23 -0300

    estrutura

--- Estrutura de Repetição/MediAlunos/src/App.java
+++ Estrutura de Repetição/MediAlunos/src/App.java
@@ -4,20 +4,12 @@ public class App {
     private static String soma;
 
     public static void main(String[] args, int j) throws Exception {
-        int n, s=0;
-        String resp;
-        Scanner teclado = new Scanner(System.in);
-        do {
-            for (int i=0; 1< args.length; i++){
-                soma = args[i];
-            }
-            System.out.println("Digite uma nota: ");
-            n = teclado.nextInt();
-            s += n; 
-            System.out.println("Quer continuar ? [S/N]: ");
-            resp = teclado.next();
-        } while (resp.equals("S"));
-        int m = s/args.length;
-        System.out.println("A media do aluno é " + m);
+        ListaCircular l = new ListaCircular();
+        l.input(10);
+        l.input(13);
+        l.input(15);
+        l.input(17);
+        l.input(18);
+        l.
     }
 }
