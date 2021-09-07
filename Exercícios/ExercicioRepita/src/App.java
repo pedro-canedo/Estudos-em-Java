3b77cf565f4c860e5fa7de9fa37d0c48596c69bf
Pedro Augusto Canedo Araujo Obalhe <82132100+pedrim775@users.noreply.github.com>
2021-09-07 20:38:30 -0300

    Swing de Vetor

---
+++ Exercícios/ExercicioRepita/src/App.java
@@ -0,0 +1,7 @@
+import javax.swing.JOprionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMenssageDialog(null, "Olá Mundo");
+    }
+}
